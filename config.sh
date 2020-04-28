40bdca62de259bb354aaa71e5fbdf55c0e4afd8a
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2020-04-28 12:18:24 +0100

    Daily data update

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
-export date="2020-04-26"
+export date="2020-04-27"
 
-export florida_date="2020-04-26-0939"
-export florida_death_page_start="32"
-export florida_death_page_end="51"
-export florida_cases_page_start="51"
-export florida_cases_page_end="561"
+export florida_date="2020-04-27-0949"
+export florida_death_page_start="31"
+export florida_death_page_end="50"
+# export florida_cases_page_start="51"
+# export florida_cases_page_end="561"
