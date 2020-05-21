ec7c05c688cd8223f2b17afe2be50df6e57f2b1f
Melodie Monod <monod.melodie@gmail.com>
2020-05-21 15:16:49 +0100

    add plot compare NYC official overall deaths to Dept of H

--- utils/make.plots.R
+++ utils/make.plots.R
@@ -10,6 +10,10 @@ death_data_ihme = read.csv(file.path("data", "official", "ihme_death_data.csv"))
 # jhu
 death_data_jhu = read.csv(file.path("data", "official", "jhu_death_data.csv"))
 
+# jhu
+death_data_nyc = read.csv(file.path("data", "official", "NYC_deaths_200518.csv"))
+
+
 # scrapped data
 path_to_data = function(state) file.path("data", last.day, "processed", paste0("DeathsByAge_", state, ".csv"))
 
@@ -75,6 +79,34 @@ make.comparison.plot = function(State, Code){
       labs(title = State, y = "Weekly deaths (overall population)") 
      ggsave(paste0("figures/comparison.ihme.jhu.depthealth_", Code, ".png"), p, w = 8, h =6)
     
+  } else if(Code == "NYC"){ 
+    death_data_nyc = data.table(death_data_nyc) %>%
+      select(Deaths, DATE_OF_INTEREST) %>%
+      rename(daily_deaths = Deaths) %>%
+      mutate(source = "City",
+             date = as.Date(DATE_OF_INTEREST, format = "%m/%d/%y"))
+    
+    death_data_scrapping = read.csv(path_to_data(Code)) %>%
+      group_by(date, code) %>%
+      summarise(daily_deaths = sum(daily.deaths)) %>%
+      mutate(source = "Dept of Health")
+    death_data_scrapping$date = as.Date(death_data_scrapping$date)
+    
+    death_data = dplyr::bind_rows(death_data_nyc, death_data_scrapping)
+    
+    p = ggplot(data = death_data, aes(x = date, y = daily_deaths, col = source)) +
+      geom_point() +
+      geom_line()  +
+      scale_x_date(date_breaks = "weeks", labels = date_format("%e %b"), 
+                   limits = c(death_data$date[1], 
+                              death_data$date[length(death_data$date)])) + 
+      theme_bw() + 
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      theme(legend.position="right")+ 
+      guides(fill = guide_legend(title="Age")) +
+      labs(title = State, y = "Daily deaths (overall population)") 
+    ggsave(paste0("figures/comparison.ihme.jhu.depthealth_", Code, ".png"), p, w = 8, h =6)
+  
   } else{
     
     death_data_ihme = data.table(subset(death_data_ihme, state_name == State)) %>%
