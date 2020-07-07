4dd999ff1bbcd7b9cfa2e688d42ec2317914aa57
YuCHENJT <43706222+YuCHENJT@users.noreply.github.com>
2020-07-08 05:52:20 +0800

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -13,3 +13,6 @@ python scripts/extraction_try.py
 GET https://opendata.arcgis.com/datasets/ebf62bbdba59497a9dba00aed0c17078_0.csv > data/$today/alaska.csv
 GET https://opendata.arcgis.com/datasets/90cc1ace62254550879f18cf94ca216b_0.csv > data/$today/colorado.csv
 GET https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.XLSX > data/$today/tn.xlsx
+
+GET https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D > data/Wisconsin.csv
+Rscript scripts/Wisconsin_extraction.R
