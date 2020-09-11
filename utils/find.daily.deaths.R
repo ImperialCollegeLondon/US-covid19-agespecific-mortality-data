508a8b5a3f99689ca997e3067fb6dc2f817fd06f
Mélodie MONOD <monod.melodie@gmail.com>
2020-09-11 14:32:40 +0100

    update processing scripts

--- utils/find.daily.deaths.R
+++ utils/find.daily.deaths.R
@@ -1,4 +1,4 @@
-find_daily_deaths = function(dates, h_data = NULL, state_code, state_name = NULL, daily.data.csv_and_xlsx = 0, json = 0, historic.data = 0)
+find_daily_deaths = function(dates, h_data, state_code)
 {
   
   first.day = min(dates)
@@ -12,79 +12,25 @@ find_daily_deaths = function(dates, h_data = NULL, state_code, state_name = NULL
     
     #
     # read daily daily update 
-    
-    #if csv or xlsx
-    if(daily.data.csv_and_xlsx){
-      file_format = ".csv"
-      if(state_code %in% c("TX")) file_format = ".xlsx"
-      file = file.path(path_to_data, Date, paste0(state_name, file_format))
-      tmp = do.call(paste0("read.", state_code, ".file"), list(file, Date))
-    }
-    #if json
-    if(json)
-    {
-      res = read_json(Date, state_name, state_code, data)
-      json_data = res[[1]]; tmp = res[[2]]
-    }
-    
-    #
-    # select Date for historical data 
-    if(historic.data)
-    {
-      tmp = subset(h_data, date == Date)
-    }
-    
+    tmp = as.data.table( subset(h_data, date == Date) )
 
     for(age_group in tmp$age){
 
-      if(json){
-        tmp = check_format_json(tmp, json_data, state_name, age_group, Date)
-      }
-      
       if(Date > first.day){
         # compute daily death
-        cum.death.t_1 = tmp[which(tmp$age == age_group & tmp$date == Date),]$cum.deaths
-        cum.death.t_0 =  data[which(data$age == age_group & data$date == (Date-1)),]$cum.deaths
+        cum.death.t_1 = tmp[age == age_group & date == Date,]$cum.deaths
+        cum.death.t_0 =  data[age == age_group & date == (Date-1),]$cum.deaths
         daily.deaths = cum.death.t_1 - cum.death.t_0 
         
         # if there is missing data at t-1
         if((Date - 1) %notin% dates){
           n.lost.days = as.numeric(Date - dates[which(dates == Date)-1] -1)
           lost.days = Date - c(n.lost.days:1)
-          cum.death.t_lag = tmp[which(tmp$age == age_group & tmp$date == Date),]$cum.deaths
-          cum.death.t_0 =  data[which(data$age == age_group & data$date == (Date-n.lost.days-1)),]$cum.deaths
+          cum.death.t_lag = tmp[age == age_group & date == Date,]$cum.deaths
+          cum.death.t_0 =  data[age == age_group & date == (Date-n.lost.days-1),]$cum.deaths
           
           # incremental deaths is distributed equally among the missing days
           daily.deaths = round((cum.death.t_lag - cum.death.t_0 )/(n.lost.days+1))
-          
-          # if the cumulative death at t0 is greater than the one at tlag
-          if( daily.deaths < 0 ) { 
-            # find dates for which we need to reduce the daily death
-            cum.deaths = cumsum(rev(data[which(data$date < Date-n.lost.days & data$age == age_group),]$daily.deaths))
-            first.date.to.reduce = rev(seq(dates[2], Date-n.lost.days-1, by = "day"))[which(cum.deaths >= abs(daily.deaths))[1]]
-            
-            # if we need to reduce the first cumulative death to obtain the required cum death at tlag
-            reduce_first_cumdeath = 0
-            if(length(which(cum.deaths >= abs(daily.deaths))) == 0){
-              reduce_first_cumdeath = 1
-              first.date.to.reduce = dates[2]
-            }
-            
-            # distribute the difference of cum death to reduce
-            for(tt in seq_along(seq(first.date.to.reduce, Date-n.lost.days-1, by = "day"))){
-              date.to.reduce = rev(seq(first.date.to.reduce, Date-n.lost.days-1, by = "day"))[tt]
-              daily.deaths.to.reduce = min(data[which(data$date == date.to.reduce & data$age == age_group),]$daily.deaths, abs(daily.deaths))
-              data[which(data$date == date.to.reduce & data$age == age_group),]$daily.deaths = data[which(data$date == date.to.reduce & data$age == age_group),]$daily.deaths - daily.deaths.to.reduce
-              daily.deaths = daily.deaths + daily.deaths.to.reduce
-            }
-              
-            if(reduce_first_cumdeath){
-              data[which(data$date == dates[1] & data$age == age_group),]$cum.deaths = max(0, data[which(data$date == dates[1] & data$age == age_group),]$cum.deaths + daily.deaths)
-              daily.deaths = 0
-            }
-
-            stopifnot(daily.deaths == 0)
-          }
           
           data = rbind(data, data.table(age = age_group, 
                                         date = lost.days, 
@@ -92,49 +38,29 @@ find_daily_deaths = function(dates, h_data = NULL, state_code, state_name = NULL
                                         daily.deaths = rep(daily.deaths, n.lost.days), 
                                         code = state_code))
           
-          # if rounding brings to 0 attribute all to the day before
-          if(daily.deaths == 0 & cum.death.t_lag == 1){
-            data[which(data$date == Date - 1& data$age == age_group),]$daily.deaths = cum.death.t_lag
+          #  if daily deaths rounded to 0
+          if(daily.deaths == 0 & (cum.death.t_lag - cum.death.t_0) > 0){
+            day.inc = which(round((cum.death.t_lag - cum.death.t_0 )/(1:(n.lost.days+1))) > 0)[1]
+            daily.deaths = round((cum.death.t_lag - cum.death.t_0 )/day.inc) 
+            data[date %in% (Date - 1:day.inc) & age == age_group,]$daily.deaths = daily.deaths
           }
+          # if the sum of the rounding death is greater than the difference
+          if(sum(rep(daily.deaths, n.lost.days+1)) > (cum.death.t_lag - cum.death.t_0)){
+            day.inc = which(sapply(1:(n.lost.days+1), function(x) sum(rep(daily.deaths, x))) <= (cum.death.t_lag - cum.death.t_0))[1]
+            if(day.inc != 1) data[date %in% (Date - c(n.lost.days:1)[-day.inc]) & age == age_group,]$daily.deaths = 0
+            if(day.inc == 1) data[date %in% (Date - c(n.lost.days:1)) & age == age_group,]$daily.deaths = 0
+          }
+          
         }
         
-        stopifnot(is.numeric(daily.deaths) & !is.null(daily.deaths))
+        stopifnot(daily.deaths >= 0)
         
-        # if the cumulative death at t-1 is greater than the one at t
-        if(daily.deaths<0){
-          # find date for which we need to reduce the daily death
-          cum.deaths = cumsum(rev(data[which(data$date < Date & data$age == age_group),]$daily.deaths))
-          first.date.to.reduce = rev(seq(dates[2], Date-1, by = "day"))[which(cum.deaths >= abs(daily.deaths))[1]]
-          
-          # if we need to reduce the first cumulative death to obtain the required cum death at tlag
-          reduce_first_cumdeath = 0
-          if(length(which(cum.deaths >= abs(daily.deaths))) == 0){
-            reduce_first_cumdeath = 1
-            first.date.to.reduce = dates[2]
-          }
-          
-          # distribute the difference of cum death to reduce
-          for(tt in seq_along(seq(first.date.to.reduce, Date-1, by = "day"))){
-            date.to.reduce = rev(seq(first.date.to.reduce, Date-1, by = "day"))[tt]
-            daily.deaths.to.reduce =  min(data[which(data$date == date.to.reduce & data$age == age_group),]$daily.deaths, abs(daily.deaths))
-            data[which(data$date == date.to.reduce & data$age == age_group),]$daily.deaths = data[which(data$date == date.to.reduce & data$age == age_group),]$daily.deaths - daily.deaths.to.reduce
-            daily.deaths = daily.deaths + daily.deaths.to.reduce
-          }
-          
-          if(reduce_first_cumdeath){
-            data[which(data$date == dates[[1]] & data$age == age_group),]$cum.deaths = max(0, data[which(data$date == dates[[1]] & data$age == age_group),]$cum.deaths + daily.deaths)
-            daily.deaths = 0
-          }
-
-          stopifnot(daily.deaths == 0)
-        }
         tmp[which(tmp$age == age_group & tmp$date == Date),]$daily.deaths = daily.deaths
       }
       
     }
     data = rbind(data, tmp)
   }
-  
   
   # overwrite cumulative deaths
   data = data %>%
--- utils/obtain.data.R
+++ utils/obtain.data.R
@@ -6,7 +6,7 @@ path_to_data = "data"
 
 `%notin%` = Negate(`%in%`)
 
-source("utils/find.daily.deaths.R")
+source("utils/summary-functions.R")
 source("utils/read.json.data.R")
 source("utils/read.daily-historical.data.R")
 
@@ -17,7 +17,7 @@ obtain.data = function(last.day, state_name, state_code, json){
   states.daily.data = c("TX", "GA", "ID", "AK", "RI")
   
   # file with entire time series
-  if(state_code %in% states.historical.data)  data = obtain.historic.data.csv_and_xlsx(last.day, state_name, state_code)
+  if(state_code %in% states.historical.data) data = obtain.historic.data.csv_and_xlsx(last.day, state_name, state_code)
   
   # file with entire time series and daily deaths (rather than cumulative deaths)
   if(state_code %in% "NM")  data = do.call(paste0("read.", state_code, ".file"), list(last.day))
@@ -47,8 +47,14 @@ obtain.daily.data.csv_and_xlsx = function(last.day, state_name, state_code){
   data_files_state = data_files[grepl(paste0(state_name, file_format), data_files)]
   dates = as.Date(gsub( ".*\\/(.+)\\/.*", "\\1", data_files_state))
   
+  # create time series
+  data = create_time_series(dates = dates, state_name = state_name, state_code = state_code, daily.data.csv_and_xlsx = TRUE)
+  
+  # ensure that cumulative death is increasing in the data
+  data = ensure_increasing_cumulative_deaths(dates = dates, h_data = data)
+  
   # find daily deaths
-  data = find_daily_deaths(dates = dates, state_name = state_name, state_code = state_code, daily.data.csv_and_xlsx = TRUE)
+  data = find_daily_deaths(dates = dates, h_data = data, state_code = state_code)
   
   return(data.table(data))
 }
@@ -63,8 +69,14 @@ obtain.historic.data.csv_and_xlsx = function(last.day, state_name, state_code){
   # find dates with data
   dates = unique(sort(tmp$date))
   
+  # create time series
+  data = create_time_series(dates = dates, h_data = tmp, state_code = state_code, historic.data = TRUE)
+  
+  # ensure that cumulative death is increasing in the data
+  data = ensure_increasing_cumulative_deaths(dates = dates, h_data = data)
+  
   # find daily deaths
-  data = find_daily_deaths(dates = dates, h_data = tmp, state_code = state_code, historic.data = 1)
+  data = find_daily_deaths(dates = dates, h_data = data, state_code = state_code)
   
   return(data.table(data))
 }
@@ -84,8 +96,14 @@ obtain.json.data = function(last.day, state_name, state_code){
   if(state_name == "alabama") dates = dates[which(dates >= as.Date("2020-05-03"))] # they changed age groups at this date
   if(state_name == "NorthCarolina") dates = dates[which(dates %notin% seq.Date(as.Date("2020-05-13"), as.Date("2020-05-19"), by = "day"))] # incorrect age groups
   
+  # create time series
+  data = create_time_series(dates = dates, state_name = state_name, state_code = state_code, json = TRUE)
+  
+  # ensure that cumulative death is increasing in the data
+  data = ensure_increasing_cumulative_deaths(dates = dates, h_data = data)
+  
   # find daily deaths
-  data = find_daily_deaths(dates = dates, state_name = state_name, state_code = state_code, json = TRUE)
+  data = find_daily_deaths(dates = dates, h_data = data, state_code = state_code)
 
   # change age label to match 5 y age bands used in the analysis
   data = modify_ageband(data, state_name,state_code)
