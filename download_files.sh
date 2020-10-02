c55253dc1553be1a6e9057ad86fb92850e458e9d
Harrison Zhu <harrisonzhu5080@gmail.com>
2020-10-02 11:21:52 +0100

    update TN

--- download_files.sh
+++ download_files.sh
@@ -19,4 +19,6 @@ GET https://data.virginia.gov/api/views/uktn-mwig/rows.csv?accessType=DOWNLOAD >
 GET https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D > data/Wisconsin.csv
 #Rscript scripts/Wisconsin_extraction.R
 python scripts/get_nm.py
-
+ 
+# TN
+GET https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.XLSX > data/tn.xlsx
