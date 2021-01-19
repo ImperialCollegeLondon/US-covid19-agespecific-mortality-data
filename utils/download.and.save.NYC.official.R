aae8aaad650fc6c64fc3e18a86bb4c2c3f18f26d
Mélodie MONOD <monod.melodie@gmail.com>
2021-01-19 22:04:58 +0000

    update off data

--- utils/download.and.save.NYC.official.R
+++ utils/download.and.save.NYC.official.R
@@ -2,7 +2,7 @@ library(RCurl)
 
 #setwd("~/git/US-covid19-data-scraping")
 
-last.updated = "210114"
+last.updated = "210118"
 
 download.file("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/trends/data-by-day.csv", 
                      destfile = file.path("data/official", paste0("NYC_deaths_", last.updated, ".csv")) ,method = "curl")
