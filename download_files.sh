de5b3bba6b87eaf3ec94becde457719755b5a46a
Melodie Monod <monod.melodie@gmail.com>
2021-01-19 11:17:14 +0000

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -14,4 +14,5 @@ GET https://data.ct.gov/resource/ypz6-8qyf.json > data/$today/connecticut_2.csv
 GET https://opendata.arcgis.com/datasets/ebf62bbdba59497a9dba00aed0c17078_0.csv > data/$today/alaska.csv
 GET https://data.virginia.gov/api/views/uktn-mwig/rows.csv?accessType=DOWNLOAD > data/$today/virginia.csv
 GET https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D > data/Wisconsin.csv
-GET https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.XLSX > data/tn.xlsx
+GET https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.XLSX > data/$today/tn.xlsx
+GET https://dshs.texas.gov/coronavirus/TexasCOVID19CaseCountData.xlsx > data/$today/texas_total.xlsx
