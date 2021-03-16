7501b12235f0286873820b98e223eca5c549b6eb
Mélodie Monod <monod.melodie@gmail.com>
2021-03-16 11:01:17 +0100

    install pipeline for CA

--- download_files.sh
+++ download_files.sh
@@ -16,3 +16,4 @@ GET https://data.virginia.gov/api/views/uktn-mwig/rows.csv?accessType=DOWNLOAD >
 GET https://opendata.arcgis.com/datasets/859602b5d427456f821de3830f89301c_11.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D > data/$today/wisconsin.csv
 GET https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.XLSX > data/$today/tn.xlsx
 GET https://dshs.texas.gov/coronavirus/TexasCOVID19CaseCountData.xlsx > data/$today/texas_total.xlsx
+GET https://data.chhs.ca.gov/dataset/f333528b-4d38-4814-bebb-12db1f10f535/resource/e2c6a86b-d269-4ce1-b484-570353265183/download/covid19casesdemographics.csv > data/$today/california.csv
