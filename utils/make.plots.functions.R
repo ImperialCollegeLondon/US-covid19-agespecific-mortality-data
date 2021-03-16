5c79c922a668018333c4c6b3be985a945ed6965e
Mélodie MONOD <monod.melodie@gmail.com>
2021-03-16 14:03:34 +0100

    update official data

--- utils/make.plots.functions.R
+++ utils/make.plots.functions.R
@@ -5,10 +5,10 @@ library(gridExtra)
 library(tidyverse)
 
 # jhu
-death_data_jhu = readRDS(file.path("data", "official", "jhu_death_data_padded_210308.rds"))
+death_data_jhu = readRDS(file.path("data", "official", "jhu_death_data_padded_210315.rds"))
 
 # NYC
-death_data_nyc = read.csv(file.path("data", "official", "NYC_deaths_210308.csv"))
+death_data_nyc = read.csv(file.path("data", "official", "NYC_deaths_210315.csv"))
 
 
 # processed data
