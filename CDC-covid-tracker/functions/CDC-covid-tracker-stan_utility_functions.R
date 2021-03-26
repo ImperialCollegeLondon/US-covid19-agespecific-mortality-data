1abf186484c87b1de8234932cbc9a3afa1353d54
Mélodie MONOD <monod.melodie@gmail.com>
2021-03-26 18:58:53 +0100

    minor fix

--- CDC-covid-tracker/functions/CDC-covid-tracker-stan_utility_functions.R
+++ CDC-covid-tracker/functions/CDC-covid-tracker-stan_utility_functions.R
@@ -6,7 +6,7 @@ add_splines_stan_data = function(stan_data, spline_degree = 3)
   
   age = stan_data$age
   stan_data$BASIS <- t(bs(age, knots=age, degree=spline_degree, intercept = TRUE)) # creating the B-splines
-  stan_data$num_basis <- nrow(B)
+  stan_data$num_basis <- nrow(stan_data$BASIS)
   
   return(stan_data)
 }
